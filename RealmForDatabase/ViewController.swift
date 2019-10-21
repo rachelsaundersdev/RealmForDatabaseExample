b5a8e2038160f11dfcf27cc18271c0345ac6c3f3
Rachel Saunders <rachelsaundersBA@gmail.com>
2019-10-21 19:53:58 +0100

    Initial Commit

---
+++ RealmForDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmForDatabase
+//
+//  Created by Rachel Saunders on 21/10/2019.
+//  Copyright © 2019 Rachel Saunders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
